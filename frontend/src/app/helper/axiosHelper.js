59291514bd2da2682a1b0d510960f04f6f223e7d
MENG Vuthvitou <liltou081@gmail.com>
2025-04-19 17:49:58 +0700

    Change path on axiosHelper.js render link api

--- frontend/src/app/helper/axiosHelper.js
+++ frontend/src/app/helper/axiosHelper.js
@@ -1,12 +1,9 @@
 import axios from "axios";
 
-const isLocal = window.location.hostname === "localhost";
-const fallbackURL = isLocal
-  ? "http://localhost:8000/api/v1"
-  : "http://192.168.88.78:8000/api/v1";
+const fallbackURL = "https://jomnay.onrender.com/api/v1";
 
 export const setUpAxios = () => {
-  axios.defaults.baseURL = import.meta.env.VITE_API_URL || fallbackURL;
+  axios.defaults.baseURL = fallbackURL;
 
   axios.interceptors.request.use(function (config) {
     const accessToken = localStorage.getItem("access_token");
