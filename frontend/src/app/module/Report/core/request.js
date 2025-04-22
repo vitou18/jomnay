2821299f596988be96d936c451f24f28c483eaa4
MENG Vuthvitou <liltou081@gmail.com>
2025-04-22 16:25:02 +0700

    Add Feature DownloadFilter by Week, Month, Year in Frontend

--- frontend/src/app/module/Report/core/request.js
+++ frontend/src/app/module/Report/core/request.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export const reqGetReport = (type) => axios.get(`/report?type=${type}`);
 
 export const reqDownloadReport = (type) =>
-  axios.get(`/report/download?type='${type}'`, {
+  axios.get(`/report/download?type=${type}`, {
     responseType: "blob",
   });
